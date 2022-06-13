fad46731040e855e41cf0ac2c36a0ae52ef1e08c
alvaroeduardo <alvaroedusilva@yahoo.com>
2022-06-12 23:28:52 -0300

    FEAT: Criação do Modal de Login | Finalização da Construção da Index Princial

--- frontEnd/src/Components/Input/index.jsx
+++ frontEnd/src/Components/Input/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import './style.css'
 
-function Input() {
-  return (
-    <div>Input</div>
-  )
+function Input(props) {
+    return (
+        <div className='inputContainer'>
+            <p className="labelInput">{ props.Label }</p>
+
+            <div className="inputContainerSetData">
+                <input placeholder={ props.Description } type={ props.Type } value={props.Value} onChange={props.OnChange}/>
+            </div>
+        </div>
+    )
 }
 
 export default Input;
--- frontEnd/src/Views/Index/index.jsx
+++ frontEnd/src/Views/Index/index.jsx
@@ -4,6 +4,7 @@ import '../../globalStyle.css';
 import './style.css';
 
 import Button from '../../Components/Button';
+import Input from '../../Components/Input';
 
 import ImageIndex from '../../Images/ImageIndex.svg';
 
@@ -23,6 +24,8 @@ const customStyles = {
 
 function index() {
     const [stateModal, setStateModal] = useState(false);
+    const [cpfValue, setCpfValue] = useState('');
+    const [pwdValue, setPwdValue] = useState('');
 
     const modalFunctions = {
         open(){
@@ -32,6 +35,14 @@ function index() {
         close(){
             setStateModal(false);
         }
+    }
+
+    function cpfHandleChange(){
+        setCpfValue(event.target.value);
+    }
+
+    function pwdHandleChange(){
+        setPwdValue(event.target.value);
     }
 
     return (
@@ -55,11 +66,34 @@ function index() {
                 isOpen={stateModal}
                 onRequestClose={modalFunctions.close}
                 style={customStyles}
+                ariaHideApp={false}
+
             >
                 <div className='loginFormContainer'>
                     <h1 className="titleLogin">Donatio</h1>
                     <h2 className="title">Login</h2>
                     <p className="loginDescription">Entre para acessar a página principal</p>
+
+                    <Input 
+                        Label="Insira seu CPF:" 
+                        Description="Somente números. Ex: 11122233344"
+                        Type="text"
+                        Value={cpfValue}
+                        OnChange={cpfHandleChange}
+                    />
+
+                    <Input 
+                        Label="Insira sua Senha:" 
+                        Description="Insira sua senha."
+                        Type="password"
+                        Value={pwdValue}
+                        OnChange={pwdHandleChange}
+                    />
+
+                    <div className="buttonBoxLogin">
+                        <a href="#">Criar Conta</a>
+                        <Button StyleType="primaryStyle" Title="Entrar"/>
+                    </div>
                 </div>
             </Modal>
         </div>
