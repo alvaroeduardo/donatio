4264f3e56b4b149412d3012ce86360ab9be80b96
alvaroeduardo <alvaroedusilva@yahoo.com>
2022-06-12 22:34:01 -0300

    FEAT: Adicionando modal

--- frontEnd/src/Views/Index/index.jsx
+++ frontEnd/src/Views/Index/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import '../../globalStyle.css';
 import './style.css';
 
@@ -6,15 +7,37 @@ import Button from '../../Components/Button';
 
 import ImageIndex from '../../Images/ImageIndex.svg';
 
-function mostraAlerta(){
-    alert("OLá mundo");
-}
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        padding: '5rem',
+        borderRadius: '50px',
+        border: 'none'
+    },
+};
 
 function index() {
+    const [stateModal, setStateModal] = useState(false);
+
+    const modalFunctions = {
+        open(){
+            setStateModal(true);
+        },
+
+        close(){
+            setStateModal(false);
+        }
+    }
+
     return (
         <div className='indexContainer'>
             <Button 
-                Action={mostraAlerta} 
+                Action={modalFunctions.open} 
                 Title={"Entrar"} 
                 StyleType={"whiteStyle buttonLogin"}
             />
@@ -27,6 +50,18 @@ function index() {
 
                 <img src={ ImageIndex } className="imageIndexContainer" />
             </div>
+
+            <Modal
+                isOpen={stateModal}
+                onRequestClose={modalFunctions.close}
+                style={customStyles}
+            >
+                <div className='loginFormContainer'>
+                    <h1 className="titleLogin">Donatio</h1>
+                    <h2 className="title">Login</h2>
+                    <p className="loginDescription">Entre para acessar a página principal</p>
+                </div>
+            </Modal>
         </div>
     )
 }
