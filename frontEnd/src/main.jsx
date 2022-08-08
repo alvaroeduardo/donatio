7f4d22f8329a23b1afd6208998e949b4238beba4
alvaroeduardo <alvaroedusilva@yahoo.com>
2022-08-08 10:20:13 -0300

    Add atualizações

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import Index from './Views/Index'
 import Home from './Views/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+    <BrowserRouter>
         <Home/>
-    </React.StrictMode>
+    </BrowserRouter>
 )
